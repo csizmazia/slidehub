4c710ecd9ab9940e4b656f5a5c5531aa7195c1f0
Stefan Csizmazia <s.csizmazia@gmail.com>
2012-11-01 21:23:14 +0100

    removed entity "slide" from datamodel statement order fixed.

--- init.sql
+++ init.sql
@@ -1,17 +1,54 @@
-/*
-SQLyog Community v9.30 
-MySQL - 5.5.16 : Database - slidehub
-*********************************************************************
-*/
+/*Table structure for table `presentation` */
 
-/*!40101 SET NAMES utf8 */;
+CREATE TABLE `presentation` (
+  `idpresentation` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `title` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`idpresentation`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=''*/;
+/*Data for the table `presentation` */
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `comment` */
+insert  into `presentation`(`idpresentation`,`timestamp`,`title`,`filename`) values (1,'2012-10-09 23:44:39','Bachelorarbeit von Stefan Csizmazia','ba_0728096');
+
+/*Table structure for table `user` */
+
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`iduser`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`iduser`,`username`,`email`) values (1,'testuser','test@mailinator.com');
+
+/*Table structure for table `note` */
+
+CREATE TABLE `note` (
+  `idnote` int(11) NOT NULL AUTO_INCREMENT,
+  `idpresentation` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `slide_no` int(10) unsigned NOT NULL,
+  `slide_x` int(11) DEFAULT NULL,
+  `slide_y` int(11) DEFAULT NULL,
+  `type` varchar(45) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idnote`),
+  KEY `fk_comment_user1` (`iduser`),
+  KEY `fk_note_presentation1` (`idpresentation`),
+  CONSTRAINT `fk_comment_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_note_presentation1` FOREIGN KEY (`idpresentation`) REFERENCES `presentation` (`idpresentation`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `note` */
+
+insert  into `note`(`idnote`,`idpresentation`,`iduser`,`content`,`slide_no`,`slide_x`,`slide_y`,`type`,`timestamp`) values (1,1,1,'lorem ipsum',1,NULL,NULL,'std','2012-11-01 21:15:42');
+
 
 CREATE TABLE `comment` (
   `idcomment` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,6 +65,17 @@ CREATE TABLE `comment` (
 
 /*Data for the table `comment` */
 
+/*Table structure for table `external_link` */
+
+CREATE TABLE `external_link` (
+  `idexternal_link` int(11) NOT NULL AUTO_INCREMENT,
+  `url` text NOT NULL,
+  `title` text,
+  PRIMARY KEY (`idexternal_link`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `external_link` */
+
 /*Table structure for table `comment_has_external_link` */
 
 CREATE TABLE `comment_has_external_link` (
@@ -41,39 +89,6 @@ CREATE TABLE `comment_has_external_link` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `comment_has_external_link` */
-
-/*Table structure for table `external_link` */
-
-CREATE TABLE `external_link` (
-  `idexternal_link` int(11) NOT NULL AUTO_INCREMENT,
-  `url` text NOT NULL,
-  `title` text,
-  PRIMARY KEY (`idexternal_link`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `external_link` */
-
-/*Table structure for table `note` */
-
-CREATE TABLE `note` (
-  `idnote` int(11) NOT NULL AUTO_INCREMENT,
-  `idslide` int(11) NOT NULL,
-  `iduser` int(11) NOT NULL,
-  `content` text NOT NULL,
-  `slide_x` int(11) DEFAULT NULL,
-  `slide_y` int(11) DEFAULT NULL,
-  `type` varchar(45) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idnote`),
-  KEY `fk_comment_slide1` (`idslide`),
-  KEY `fk_comment_user1` (`iduser`),
-  CONSTRAINT `fk_comment_slide1` FOREIGN KEY (`idslide`) REFERENCES `slide` (`idslide`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comment_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-/*Data for the table `note` */
-
-insert  into `note`(`idnote`,`idslide`,`iduser`,`content`,`slide_x`,`slide_y`,`type`,`timestamp`) values (1,1,1,'testcomment',NULL,NULL,'std','2012-11-01 13:56:12');
 
 /*Table structure for table `note_has_external_link` */
 
@@ -89,50 +104,6 @@ CREATE TABLE `note_has_external_link` (
 
 /*Data for the table `note_has_external_link` */
 
-/*Table structure for table `presentation` */
-
-CREATE TABLE `presentation` (
-  `idpresentation` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `title` varchar(255) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`idpresentation`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-/*Data for the table `presentation` */
-
-insert  into `presentation`(`idpresentation`,`timestamp`,`title`,`filename`) values (1,'2012-10-09 23:44:39','Bachelorarbeit von Stefan Csizmazia','ba_0728096');
-
-/*Table structure for table `slide` */
-
-CREATE TABLE `slide` (
-  `idslide` int(11) NOT NULL AUTO_INCREMENT,
-  `idpresentation` int(11) NOT NULL,
-  `title` text,
-  `page_number` int(11) NOT NULL,
-  PRIMARY KEY (`idslide`),
-  KEY `fk_slide_presentation` (`idpresentation`),
-  CONSTRAINT `fk_slide_presentation` FOREIGN KEY (`idpresentation`) REFERENCES `presentation` (`idpresentation`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-/*Data for the table `slide` */
-
-insert  into `slide`(`idslide`,`idpresentation`,`title`,`page_number`) values (1,1,'was auch immer',1);
-
-/*Table structure for table `user` */
-
-CREATE TABLE `user` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`iduser`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-
-/*Data for the table `user` */
-
-insert  into `user`(`iduser`,`username`,`email`) values (1,'testuser','test@mailinator.com');
-
 /*Table structure for table `vote` */
 
 CREATE TABLE `vote` (
@@ -142,12 +113,8 @@ CREATE TABLE `vote` (
   PRIMARY KEY (`iduser`,`idnote`),
   KEY `fk_user_has_comment_comment1` (`idnote`),
   KEY `fk_user_has_comment_user1` (`iduser`),
-  CONSTRAINT `fk_user_has_comment_comment1` FOREIGN KEY (`idnote`) REFERENCES `note` (`idnote`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_comment_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_user_has_comment_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_comment_comment1` FOREIGN KEY (`idnote`) REFERENCES `note` (`idnote`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `vote` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
